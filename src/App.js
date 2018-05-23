2984bc3f523e0ef74b556d3098826bb55c8e417c
Ethan Sternke <ethan.sternke@gmail.com>
2018-05-23 10:12:46 -0400

    moved logic to component in App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,19 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Text extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: props.color,
+      value: props.value,
+    }
+  }
+  render() {
+   return <div style={{color: this.state.color}}>{this.state.value}</div>
+  }
+}
+
 class App extends Component {
   render() {
     return (
@@ -13,6 +26,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>Hello, <Text color="green" value="Kenzie" /></h1>
+        <h1>This is <Text color="red" value="Ethan" /></h1>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import register from './registerServiceWorker';
 
 function Text(props) {
     return <h1><span style={{color: props.color}}>{props.value}</span></h1>;
 }
   
 const element = <Text color="red" value="Hello World! You suck! Sincerely, Ethan." />;
-ReactDOM.render(
-    element,
-    document.getElementById('root')
-);
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
+
+// ReactDOM.render(
+//     element,
+//     document.getElementById('root')
+// );
